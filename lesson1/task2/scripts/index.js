c5af692688304e2e4767ebe7ad4f0e6000b58a3e
“Il_Roman” <il.roman@ukr.net>
2020-02-02 18:37:53 +0200

    commit

---
+++ lesson1/task2/scripts/index.js
@@ -0,0 +1,5 @@
+import { createTask } from './createTasks.js';
+import { renderListItems } from './renderListItems.js';
+import { onStorageChange, onDocumentLoaded } from './onStorageChange.js';
+import { onToggleTask } from './onToggleTask.js';
+import { deleteEvent } from './deleteTask.js';
