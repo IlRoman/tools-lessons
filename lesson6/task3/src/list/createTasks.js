958a4ff1db138dc9bd2ef8b29a602e6cd2f510a4
“Il_Roman” <il.roman@ukr.net>
2020-02-05 16:26:53 +0200

    commit

---
+++ lesson6/task3/src/list/createTasks.js
@@ -0,0 +1,23 @@
+import { renderListItems } from './renderListItems.js';
+import { createNewTask, getTasksList } from './tasksGetWay.js'
+
+let createTaskBtn = document.querySelector('.create-task-btn');
+createTaskBtn.addEventListener('click', createTask);
+
+export function createTask() {
+    let task_input = document.querySelector('.task-input');
+
+    const newTask = {
+        text: task_input.value,
+        done: false,
+        id: Math.trunc(Math.random() * 1000),
+        date: new Date()
+    }
+
+    createNewTask(newTask)
+        .then(() => getTasksList())
+        .then(tasks => {
+            localStorage.setItem('tasksList', JSON.stringify(tasks))
+            renderListItems(tasks);
+        })
+}
